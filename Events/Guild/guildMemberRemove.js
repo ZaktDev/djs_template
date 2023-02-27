33728427a122ffa98c9ba60db362d5a01302d31f
ZaktDev <126408828+ZaktDev@users.noreply.github.com>
2023-02-27 18:04:19 +0100

    Update guildMemberRemove.js

--- Events/Guild/guildMemberRemove.js
+++ Events/Guild/guildMemberRemove.js
@@ -4,25 +4,20 @@ const { GuildMember } = require("discord.js");
 
 module.exports = {
   name: "guildMemberRemove",
-  /**
-   * @param {GuildMember} member
-   */
-  execute(member) {
+
+  async execute(member) {
     const {user, guild} = member;
-    const memberLogs = member.guild.channels.cache.get('1008752273012314233');
-    const leaveMessage = `<@${member.id}> left! It was nice having you around.`;
+    const c = member.guild.channels.cache.get('');
+    const leaveMessage = `<@${member.id}> se fue.`;
     
     const leaveEmbed = new EmbedBuilder()
-    .setTitle('**:x: Member Left :x: **')
     .setColor(0xf54242)
     .setDescription(leaveMessage)
     .addFields(
-      { name:'Total member count:', value: `${guild.memberCount}` }
+      { name:'Miembros:', value: `${guild.memberCount}` }
     )
-    .setThumbnail('https://cdn.discordapp.com/attachments/754370302279942356/1008371801530847293/kajdev.png')
     .setTimestamp();
 
-    memberLogs.send({embeds: [leaveEmbed]});
-    console.log(`${member.id} left the guild.`)
+    c.send({embeds: [leaveEmbed]});
   },
 };
