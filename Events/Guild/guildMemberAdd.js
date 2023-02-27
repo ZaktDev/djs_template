bc9d39359e38220809fd2c86d06ca84ae6a15950
ZaktDev <126408828+ZaktDev@users.noreply.github.com>
2023-02-27 18:02:44 +0100

    Update guildMemberAdd.js

--- Events/Guild/guildMemberAdd.js
+++ Events/Guild/guildMemberAdd.js
@@ -3,25 +3,21 @@ const {GuildMember} = require("discord.js");
 
 module.exports = {
   name: "guildMemberAdd",
-  /**
-   * @param {GuildMember} member
-   */
-  execute(member) {
+
+  async execute(member) {
     const {user, guild} = member;
-    const memberLogs = member.guild.channels.cache.get('1008752273012314233');
-    const welcomeMessage = `Welcome <@${member.id}> to **kajdev**! Let's make some stuff together!`;
+    const c = await guild.channels.cache.get("")
+    const welcomeMessage = `Bienvenido <@${member.id}>`;
     
     const welcomeEmbed = new EmbedBuilder()
-    .setTitle('**:partying_face: New member :partying_face: **')
     .setColor(0x4ea3f7)
     .setDescription(welcomeMessage)
     .addFields(
-      { name:'Total member count:', value: `${guild.memberCount}` }
+      { name:'Miembros:', value: `${guild.memberCount}` }
     )
-    .setThumbnail('https://cdn.discordapp.com/attachments/754370302279942356/1008371801530847293/kajdev.png')
     .setTimestamp();
 
-    member.roles.add('1008745069223870484');
+
     memberLogs.send({embeds: [welcomeEmbed]});
     console.log(`${member.id} joined the guild.`)
   },
