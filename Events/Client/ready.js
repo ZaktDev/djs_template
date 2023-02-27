169f81ac196821383f1add60335ba0dc6c908770
ZaktDev <126408828+ZaktDev@users.noreply.github.com>
2023-02-27 18:05:55 +0100

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,13 +1,10 @@
-const {Client, ActivityType} = require("discord.js");
+const {Client} = require("discord.js");
 
 module.exports = {
   name: "ready",
   once: true,
-  /**
-   *
-   * @param {Client} client
-   */
-  execute(client) {
-    console.log(`${client.user.username} booted up.`);
+  
+  async execute(client) {
+    console.log(`${client.user.username} se inicio.`);
   },
 };
